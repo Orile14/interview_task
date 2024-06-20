707495ee7d240430255fed1423b8991991977fb1
Ori Levy <orile14@gmail.com>
2024-06-20 21:13:31 +0300

    second draft, reformatting and making it modular

--- interview_task.py
+++ interview_task.py
@@ -1,54 +1,50 @@
 file_path = "release_notes.txt"
-unique_element = "Issue"
-second_element = "Title"
 
-
-def findMissingNotes(filename):
+def findMissingElements(missing_element, filename):
     flag = False
     file = open(filename, 'r')
     lines = file.readlines()
-    missing_notes = []
+    missing_elements = []
     current_note = None
     for line in lines:
         if "Owner" in line and flag:
             flag = False
             owner = line.split(":")[1].strip()
             if owner not in ["chdauto", "syspdbuild", "syspdbuild2"]:
-                missing_notes.append((current_note, owner))
+                missing_elements.append((current_note, owner))
             else:
-                if missing_notes:
-                    missing_notes.pop()
-        if "[ NOTE_MISSING ]" in line:
+                if missing_elements:
+                    missing_elements.pop()
+        if missing_element in line:
             current_note = line.split("#")[1].strip()
             flag = True
-    return missing_notes
+    return missing_elements
 
 
-def findUniqeIssues(filename):
-    issues = set()
-    cur_title = ""
+def findUniqeElements(unique_element, second_element, filename):
+    elements = set()
+    cur_element = ""
     file = open(filename, 'r')
     lines = file.readlines()
     for line in lines:
         if second_element in line:
-            cur_title = line.split(":")[1].strip()
+            cur_element = line.split(":")[1].strip()
         if unique_element in line:
-            if line.split(":")[1].strip() not in issues:
-                issues.add((line.split(":")[1].strip(), cur_title))
+            if line.split(":")[1].strip() not in elements:
+                elements.add((line.split(":")[1].strip(), cur_element))
 
-    return issues
-
-
-def printSummary(issues, missing_notes):
-    for issue in issues:
-        print("[" + issue[0] + "]", issue[1])
-    print("")
-    print("Commits with missing git notes:")
-    for note in missing_notes:
-        print("[ NOTE_MISSING ] Commit #" + note[0])
-        print("@" + note[1] + ", Please add git notes to this commit!")
+    return elements
 
 
-issues = findUniqeIssues(file_path)
-missing_notes = findMissingNotes(file_path)
+def printSummary(unique_elements, missing_elements):
+    for element in unique_elements:
+        print("[" + element[0] + "]", element[1])
+    print("\nCommits with missing git notes:")
+    for element in missing_elements:
+        print("[ NOTE_MISSING ] Commit #" + element[0])
+        print("@" + element[1] + ", Please add git notes to this commit!")
+
+
+issues = findUniqeElements("Issue", "Title", file_path)
+missing_notes = findMissingElements("[ NOTE_MISSING ]", file_path)
 printSummary(issues, missing_notes)
